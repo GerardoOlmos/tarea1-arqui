1c8e5b424e74c932ff1c36d7db3aa91c5be8b490
Gerardo <gaolmos@uc.cl>
2016-09-11 20:30:12 -0300

    Add permission to log

---
+++ tarea1/vendor/bundle/ruby/2.3.0/gems/nokogiri-1.6.8/ext/nokogiri/tmp/x86_64-apple-darwin15.4.0/ports/libxml2/2.9.4/libxml2-2.9.4/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/gerardoolmos/Downloads/10mo SEM 2016/arqui/tarea1dir/tarea1app/tarea1-arqui/tarea1/vendor/bundle/ruby/2.3.0/gems/nokogiri-1.6.8/ports/x86_64-apple-darwin15.4.0/libxml2/2.9.4/lib"
+XML2_LIBS="-lxml2 -lz  -lpthread  -liconv  -lm "
+XML2_INCLUDEDIR="-I/Users/gerardoolmos/Downloads/10mo SEM 2016/arqui/tarea1dir/tarea1app/tarea1-arqui/tarea1/vendor/bundle/ruby/2.3.0/gems/nokogiri-1.6.8/ports/x86_64-apple-darwin15.4.0/libxml2/2.9.4/include/libxml2"
+MODULE_VERSION="xml2-2.9.4"
+
